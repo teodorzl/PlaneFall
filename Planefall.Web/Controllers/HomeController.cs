bbbb661179622bc598a7feb6ae5eda85b21bbdd0
Teodor Zlatkov <35622940+teodorzl@users.noreply.github.com>
2020-02-18 20:54:23 +0200

    Clean up HomeController

--- Planefall.Web/Controllers/HomeController.cs
+++ Planefall.Web/Controllers/HomeController.cs
@@ -7,19 +7,7 @@
 
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            this._logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return this.View();
-        }
-
-        public IActionResult Privacy()
         {
             return this.View();
         }
