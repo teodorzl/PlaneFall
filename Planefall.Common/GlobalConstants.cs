0009973f0d522f1acc6c0c3a0965c959f4fb2735
Teodor Zlatkov <35622940+teodorzl@users.noreply.github.com>
2020-02-29 16:25:30 +0200

    Improve home page

--- Planefall.Common/GlobalConstants.cs
+++ Planefall.Common/GlobalConstants.cs
@@ -5,5 +5,6 @@ namespace Planefall.Common
         public const string AdministratorRoleName = "Administrator";
         public const string TempDataErrorMessageKey = "ErrorMessage";
         public const string TempDataSuccessMessageKey = "SuccessMessage";
+        public const string DisableContainerKey = "SuccessMessage";
     }
 }
